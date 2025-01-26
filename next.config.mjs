7a1b4bbb33c2f6ed764eea27c43f9ee71046d05e
Allan Ayford <60179207+allanayford-dev@users.noreply.github.com>
2025-01-26 19:42:32 +0200

    Test 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_BASE_URL:
+      process.env.NEXT_PUBLIC_API_BASE_URL || "https://fallback-url.com",
+  },
 };
 
 export default nextConfig;
